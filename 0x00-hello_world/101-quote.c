78e57fc479371c80f4ed37e5674d23496e56b3c2
MohamedCk00 <mohamedchakir00@gmail.com>
2023-03-16 12:07:51 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,11 @@
+#include <stdio.h>
 #include <unistd.h>
-
 /**
- * main - prints an error message to stderr
- *
+ * main -Entry
  * Return: 1
- */
+*/
 int main(void)
 {
-	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t len = 59;
-	
-	/* write the message to stdout */
-	if (write(1, msg, len) != len)
-        return 1;
-
-	/* print the length of the message to stdout */
-	printf("[Got]:\n%.*s", 7, msg);
-	printf("(%d chars long)\n", 7);
-
-	/* print an empty stderr message */
-	fprintf(stderr, "[stderr]: [Anything]\n");
-
-	return 1;
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
