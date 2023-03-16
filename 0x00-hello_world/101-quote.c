0620e0b2fc78d12331fe711290077aeed417d6e1
MohamedCk00 <mohamedchakir00@gmail.com>
2023-03-16 11:41:40 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,9 +8,18 @@
 int main(void)
 {
 	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	ssize_t len = sizeof(msg) - 1;
-	/* write the message to stderr */
-	if (write(2, msg, len) != len)
-		return 1;
+	ssize_t len = 59;
+	
+	/* write the message to stdout */
+	if (write(1, msg, len) != len)
+        return 1;
+
+	/* print the length of the message to stdout */
+	printf("[Got]:\n%.*s", 7, msg);
+	printf("(%d chars long)\n", 7);
+
+	/* print an empty stderr message */
+	fprintf(stderr, "[stderr]: [Anything]\n");
+
 	return 1;
 }
