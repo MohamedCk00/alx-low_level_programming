d1301e03caf3019bbafd1c56b6e4ac965527c3e7
MohamedCk00 <mohamedchakir00@gmail.com>
2023-03-17 08:53:07 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,31 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
 /**
- * main - main function
- * Return: always 0
+ * main - return a combination of number
+ * Return: return 0 if suceessful
 */
 int main(void)
 {
 	int i, j;
 
-	for (i = 0; i < 99; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = i + 1; j <= 99; j++)
+		for (j = 0; j < 100; j++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
+			putchar((i / 10) + 48);
+			putchar((i % 10) + 48);
 			putchar(' ');
-			putchar((j / 10) + '0');
-			putchar((j % 10) + '0');
-			if (i != 99 || j != 99)
+			putchar((j / 10) + 48);
+			putchar((j % 10) + 48);
+			if (i != 98 || j != 99)
 			{
 				putchar(',');
 				putchar(' ');
 			}
 		}
 	}
-
+	putchar('\n');
 	return (0);
 }
